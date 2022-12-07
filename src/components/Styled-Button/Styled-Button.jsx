5955ac06dbb20d31e9419aacdbe4bb3f6b7caffb
Kian G <kian.gormley13@aol.com>
2022-12-07 15:11:18 -0500

    Styling next button component for every page
    Solid progress just need to get the arrow inside

---
+++ src/components/Styled-Button/Styled-Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styled-Button.css';
+
+export default function StyledButton () {
+  return (
+    <div className="next-page">
+      <button type="button" className="styled-button">arrow</button>
+      <p className="next">Next</p>
+    </div>
+
+  )
+}
