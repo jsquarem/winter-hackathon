38a13e31e645c435b19e5942d3b316f1bdc6c480
youngjerald239 <youngjerald239@gmail.com>
2022-12-07 17:22:17 -0600

    adjusted navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import './NavBar.css'
 
 export default function NavBar() {
   return (
