ae32a8f1d08f349ec9bd868a4753269a5067d045
Kian G <kian.gormley13@aol.com>
2022-12-07 16:59:19 -0500

    Bootstrap arrow
    successfully imported, colored, and placed in middle of button

--- src/components/Styled-Button/Styled-Button.jsx
+++ src/components/Styled-Button/Styled-Button.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Styled-Button.css';
+import { ArrowRight } from 'react-bootstrap-icons';
 
 export default function StyledButton () {
   return (
     <div className="next-page">
-      <button type="button" className="styled-button">arrow</button>
+      <button type="button" className="styled-button"></button>
+      <ArrowRight className="arrow-right" size={20} />
       <p className="next">Next</p>
     </div>
 
