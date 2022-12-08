5616ee1ad6b7a9d10974e220644e7d70071a1910
Kian G <kian.gormley13@aol.com>
2022-12-07 22:01:21 -0500

    project overview files changes
    projectoverview.jsx and projectoverview.css work

--- src/pages/Project/ProjectOverview.jsx
+++ src/pages/Project/ProjectOverview.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
-import Project from './Project'
+import React from 'react';
+import Project from './Project';
+import sampleProject from './sampleProject';
+import './ProjectOverview.css';
 
 export default function ProjectOverview() {
   return (
-    <div>ProjectOverview</div>
+    <div class="project-overview">
+    <div class="proj-1">
+      
+    <h6>Project Title</h6>
+    <p>{sampleProject.projectTitle}</p>
+    </div>
+    <div class="proj-2">
+
+    <h6>Project Description</h6>
+    <p>{sampleProject.description}</p>
+    </div>
+    <div class="proj-3">
+
+    <h6>School</h6>
+    <p>{sampleProject.school}</p>
+    </div>
+    <div class="proj-4">
+    <h6>My Wishlist</h6>
+    </div>
+    <p id="wish-item">{sampleProject.projectWishList}</p>
+    <button type="button" className="publish-btn"></button>
+    
+    </div>
   )
 }
