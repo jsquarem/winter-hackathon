6f8a894d8c5712064f10a4f47a11c3f883471bc1
Kian G <kian.gormley13@aol.com>
2022-12-07 22:33:00 -0500

    projectoverview some changes to jsx and css

--- src/pages/Project/ProjectOverview.jsx
+++ src/pages/Project/ProjectOverview.jsx
@@ -6,26 +6,27 @@ import './ProjectOverview.css';
 export default function ProjectOverview() {
   return (
     <div class="project-overview">
-    <div class="proj-1">
-      
+    <div id="proj-1">
     <h6>Project Title</h6>
     <p>{sampleProject.projectTitle}</p>
     </div>
-    <div class="proj-2">
 
+    <div id="proj-2">
     <h6>Project Description</h6>
     <p>{sampleProject.description}</p>
     </div>
-    <div class="proj-3">
 
+    <div id="proj-3">
     <h6>School</h6>
     <p>{sampleProject.school}</p>
     </div>
-    <div class="proj-4">
+
+    <div id="proj-4">
     <h6>My Wishlist</h6>
-    </div>
     <p id="wish-item">{sampleProject.projectWishList}</p>
+    </div>
     <button type="button" className="publish-btn"></button>
+
     
     </div>
   )
