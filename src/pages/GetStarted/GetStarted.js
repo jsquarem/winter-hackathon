5b35ca3627d2a43ac627ffb482736ea0f242fec5
youngjerald239 <youngjerald239@gmail.com>
2022-12-07 18:29:26 -0600

    updated get started and home page

--- src/pages/GetStarted/GetStarted.js
+++ src/pages/GetStarted/GetStarted.js
@@ -5,26 +5,34 @@ import './GetStarted.css'
 function GetStarted() {
   return (
       <div>
+          <div className="circle5"></div>
+          <div className="circle6"></div>
 
+        <div className="text2">
           <h1>Connect to donors.</h1>
-          <h3>Engage with your community.</h3>
-          <h3>Empower Education.</h3>
-          <h3>Connect students and classrooms to</h3>
-          <h3>the essential supplies they need.</h3>
+          <h2>Engage with your community.</h2>
+          <h2>Empower Education.</h2>
+          <h2>Connect students and classrooms to</h2>
+          <h2>the essential supplies they need.</h2>
+          </div>
+          <div className="center">
+          <div className="circle7"></div>
           <div className="mb-2">
               <Button variant="primary" size="lg">
                   Get Started
               </Button>{' '}
           </div>
+          </div>
           <h2>How it works.</h2>
 
           <div>
               <ol>
-                  <li>Request Items</li> Create a project with a wish list for your students.
+                  <li>Request Items</li> <p>Create a project with a wish list for your students.</p>
                   <li>Get Funded</li> Donors purchase the items on your wish list.
                   <li>Receive the items</li> Items will be shipped directly to your school.
               </ol>
           </div>
+          
       </div>
   )
 }
