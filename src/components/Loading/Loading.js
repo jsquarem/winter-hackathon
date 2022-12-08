13732c9fc0c446b10ce3aec78004e51beaf2cb1a
youngjerald239 <youngjerald239@gmail.com>
2022-12-07 22:01:27 -0600

    updated loading screen

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ReactLoading from "react-loading";
+import './Loading.css'
 
 export default function Loading() {
     return (
-        <div>
-            <h2>We're on our way!</h2>
-            <ReactLoading type="bubbles" color="#0000FF"
-                height={100} width={50} />
+        <div className="loader">
+            <h2 className="loadtext">We're on our way!</h2>
+            <ReactLoading className="loadbubble" type="bubbles" color="#BDD9BF"
+                height={100} width={150} />
 
             <img
                 className="t-rex"
                 src="https://i.pinimg.com/originals/ee/de/0e/eede0eccaf59ddda3e629f98bbfd0a10.gif"
                 alt="t-rex"
             />
-            <h2>Sending you back to your profile page...</h2>
+            <h2 className="loadtext">Sending you back to your profile page...</h2>
         </div>
     );
 }
