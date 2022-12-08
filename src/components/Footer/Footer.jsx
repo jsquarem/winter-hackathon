1958770bd9b007e5fe816b908d3efa0199d3b571
youngjerald239 <youngjerald239@gmail.com>
2022-12-08 09:23:34 -0600

    set up footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { SlSocialTumblr, SlSocialYoutube } from "react-icons/sl"
+import { AiFillLinkedin, AiFillFacebook } from "react-icons/ai"
+import "./Footer.css"
 
 export default function Footer() {
-  return <div><Navbar bg="dark" variant="dark">
-    <Container>
-      <Navbar.Brand href="/"><img src="https://catcollection7-11.s3.us-east-2.amazonaws.com/teachaid-logo-dark.png" /></Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#features">Features</Nav.Link>
-        <Nav.Link href="#pricing">Pricing</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
-    </div>;
+  return (
+    <div>
+      <Navbar className="footernav" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand className="brand" href="/"><img src="https://catcollection7-11.s3.us-east-2.amazonaws.com/teachaid-logo-dark.png" /></Navbar.Brand>
+          <Nav className="me-auto">
+          <Nav.Link href="https://tumblr.com"><SlSocialTumblr /></Nav.Link>
+          <Nav.Link href="https://youtube.com"><SlSocialYoutube/></Nav.Link>
+          <Nav.Link href="https://linkedin.com"><AiFillLinkedin/></Nav.Link>
+          <Nav.Link href="https://facebook.com"><AiFillFacebook /></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  )
 }
